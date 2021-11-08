e52a6b572fe93be1986c3288ab3b494a69d7030c
Fola Flor <dpcalfola@gmail.com>
2021-11-08 23:15:32 +0900

    1st commit
    Q1000, Q10869

---
+++ BackJoonJava/src/q2021year/Q1000.java
@@ -0,0 +1,20 @@
+package q2021year;
+
+import java.util.Scanner;
+
+public class Q1000 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+//        System.out.println(a);
+//        System.out.println(b);
+        int result = a + b;
+
+        System.out.println(result);
+
+
+    }
+}
---
+++ BackJoonJava/src/q2021year/Q10869.java
@@ -0,0 +1,17 @@
+package q2021year;
+
+import java.util.Scanner;
+
+public class Q10869 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
