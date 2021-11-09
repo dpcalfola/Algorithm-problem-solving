f8c5a5a9d6dc03af8f9e0cbc9a28dcebc6317c49
Fola Flor <dpcalfola@gmail.com>
2021-11-09 18:58:38 +0900

    2nd commit
    Q2438, Q10430, Q2739 Q14681

---
+++ BackJoonJava/src/myApp/DateRecordApp.java
@@ -0,0 +1,23 @@
+package myApp;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateRecordApp {
+    public static void main(String[] args) throws IOException {
+        Date today = new Date();
+
+        SimpleDateFormat date = new SimpleDateFormat("EEEE, MMMM, dd, yyyy : ");
+        System.out.println("Recorde in text : " + date.format(today));
+
+        FileWriter fw = new FileWriter("TodayDateRecorde.txt", true);
+
+
+        fw.write("\n" + date.format(today));
+        fw.close();
+
+
+    }
+}
