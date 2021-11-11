dbd066bb3244cf1304d3d4e0623aa7d43be18fc3
Fola Flor <dpcalfola@gmail.com>
2021-11-11 23:16:23 +0900

    4th commit
    Q1001

---
+++ BackJoonJava/src/q2021year/Q10952_faild.java
@@ -0,0 +1,32 @@
+package q2021year;
+
+import java.util.Scanner;
+
+public class Q10952_faild {
+    static int repeat = 1;
+    static int[] result = new int[repeat];
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = -1;
+        int b = -1;
+
+
+        while (a != 0 && b != 0) {
+
+            for (int i = 0; i <= repeat; i++) {
+
+                a = scan.nextInt();
+                b = scan.nextInt();
+                result[i] = a + b;
+                repeat++;
+            }
+
+        }
+        for (int i = 0; i <= repeat; i++) {
+            System.out.println(result[i]);
+        }
+
+
+    }
+}
