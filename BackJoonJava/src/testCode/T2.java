079dff33471fa55bd7eab8cd7c7235032248b583
Fola Flor <dpcalfola@gmail.com>
2021-11-25 17:43:50 +0900

    Q1212

--- BackJoonJava/src/testCode/T2.java
+++ BackJoonJava/src/testCode/T2.java
@@ -4,6 +4,10 @@ public class T2 {
 
 
     public static void main(String[] args) {
-        System.out.println(3/2);
+        StringBuilder sb = new StringBuilder();
+        sb.append("a");
+
+        StringBuilder sb2 = new StringBuilder();
+
     }
 }
