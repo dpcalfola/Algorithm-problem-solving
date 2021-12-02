5cfc3464ab44b72cf8beec703e04c6884f5cd36c
dpcalfola <89129145+dpcalfola@users.noreply.github.com>
2021-12-02 22:26:02 +0900

    java : Q2920

---
+++ BackJoonJava/src/q2021year/Q2869_failed.java
@@ -0,0 +1,42 @@
+package q2021year;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2869_failed {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+
+        int result = 0;
+
+        if (a >= c) {
+            result = 1;
+
+        } else if ((a + b) >= c) {
+            result = ((c - b) / (a - b)) + 1;
+
+        } else {
+            result = (c - b) / (a - b);
+        }
+
+        System.out.println(result);
+
+
+    }
+}
+
+
+/* a: 올라가는 길이
+ *  b: 떨어지는 길이
+ *  c: 전체 길이
+ * */
---
+++ BackJoonJava/src/q2021year/Q2869_retry.java
@@ -0,0 +1,35 @@
+package q2021year;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2869_retry {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+
+        int result ;
+
+
+
+
+
+    }
+}
+
+/* a: 올라가는 길이
+*  b: 떨어지는 길이
+*  c: 전체 길이
+* */
