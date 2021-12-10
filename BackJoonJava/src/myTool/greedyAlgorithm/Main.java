c7a02396eb79f719e48440364a6166d472ea200d
Fola Flor <dpcalfola@gmail.com>
2021-12-10 21:05:57 +0900

    Revert "Revert "Q5585 Q2501""
    This reverts commit f5c00d12d01446961698451eb256bc93f0211cd1.

--- BackJoonJava/src/myTool/greedyAlgorithm/Main.java
+++ BackJoonJava/src/myTool/greedyAlgorithm/Main.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         GreedyAlgorithm g1 = new GreedyAlgorithm();
 
-        g1.targetNumber = 124 ;
-        g1.unitArr = new int[]{25, 10, 1, 5};
+        g1.targetNumber = 18 ;
+        g1.unitArr = new int[]{5, 3};
 
         int[] answerArr = g1.runGreedy();
 
         for(int j : answerArr){
-            System.out.print(j);
+            System.out.println(j);
         }
+
+        System.out.println(g1.checkReminder);
 
 
     }
