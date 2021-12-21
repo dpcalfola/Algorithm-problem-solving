349f0d591464b9d6b3ba1a48dc71cbebcf59c2f1
Fola Flor <dpcalfola@gmail.com>
2021-12-21 21:27:35 +0900

    java : Q5554

---
+++ BackJoonJava/src/myTool/PrintAllArray.java
@@ -0,0 +1,15 @@
+package myTool;
+
+public class PrintAllArray {
+
+    public static void printAllStr(String[] arr ) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+
+
+    }
+}
+
---
+++ BackJoonJava/src/q2021year/Q5554.java
@@ -0,0 +1,25 @@
+package q2021year;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q5554 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0 ;
+
+        for (int i = 0; i < 4; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+
+        int min = sum/60;
+        int sec = sum%60;
+
+
+        System.out.println(min);
+        System.out.println(sec);
+    }
+}
