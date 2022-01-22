1b554732c7c110f9a84e881b9612d478d325cf23
Fola Flor <dpcalfola@gmail.com>
2022-01-22 13:54:52 +0900

    solve >
    swift : Q15873

--- BackJoonSwift/BackJoonSwift2021y/BackJoonQGroup/BackJoonQGroup/main.swift
+++ BackJoonSwift/BackJoonSwift2021y/BackJoonQGroup/BackJoonQGroup/main.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-let inA = readLine()!.components(separatedBy: " ").map{ Int($0)! }
-let inB = readLine()!.components(separatedBy: " ").map{ Int($0)! }.sorted()
 
-print(inB[inA[1]-1])
+
+
+
 
 
 
