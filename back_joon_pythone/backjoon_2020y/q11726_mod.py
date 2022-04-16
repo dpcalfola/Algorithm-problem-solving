59a87ab1670c431e91fd2b590f06feba7fa1e45f
Fola Flor <dpcalfola@gmail.com>
2022-04-16 20:25:36 +0900

    Beckjoon python (2002/04/16) (2)

---
+++ back_joon_pythone/backjoon_2020y/q11726_mod.py
@@ -0,0 +1,14 @@
+import sys
+
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
+
+N = int(sys.stdin.readline().rstrip())
+
+for i in range(3, 1001):
+    # dp[i-1] : 마지막 타일이 세로로 1개
+    # dp[i-2] : 마지막 타일이 가로로 2개
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+
+print(dp[N])
