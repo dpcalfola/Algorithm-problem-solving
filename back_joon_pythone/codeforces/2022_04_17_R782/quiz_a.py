a003d090e2eb056307d47a1d0c1a3be23712e4af
Fola Flor <dpcalfola@gmail.com>
2022-04-18 13:14:22 +0900

    Codeforces python Round #782 div.2 (2022/04/17)

---
+++ back_joon_pythone/codeforces/2022_04_17_R782/quiz_a.py
@@ -0,0 +1,22 @@
+import sys
+
+t = int(sys.stdin.readline().rstrip())
+
+for _ in range(t):
+    n, r, b = map(int, sys.stdin.readline().split())
+    arr = []
+    col = r // (b + 1)
+    reminder = r % (b + 1)
+    for _ in range(b + 1):
+        arr.append('R' * col)
+        arr.append('B')
+    arr.pop()
+    print('reminder :', reminder)
+    print(arr)
+    step = 1
+    for _ in range(reminder):
+        arr.insert(step, 'R')
+        step += 2
+        step += 1
+    print(arr)
+    print(''.join(arr))
---
+++ back_joon_pythone/codeforces/2022_04_17_R782/test.py
@@ -0,0 +1,11 @@
+s = '12345'
+s = s[:-1:]
+print(s)
+
+index_4 = s.index('4')
+print(index_4)
+
+arr = [1, 2, 3, 4, 5, 6]
+print(arr)
+arr.insert(3, 0)
+print(arr)
