fcf4c44bb9119b40ffe6351d340bad7f385bdd8c
Fola Flor <dpcalfola@gmail.com>
2022-04-29 21:32:30 +0900

    python ) baekjoon 2022_04_29

---
+++ baekjoon_pythone/baekjoon_2020y/q9733_failed.py
@@ -0,0 +1,28 @@
+import collections
+import sys
+
+input_1 = ''
+input_2 = ''
+
+try:
+    input_1 = sys.stdin.readline()
+except EOFError:
+    pass
+
+try:
+    input_2 = sys.stdin.readline()
+except EOFError:
+    pass
+
+arr = list(input_2.split()) + list(input_1.split())
+dic_counter = collections.Counter(arr)
+
+total = len(arr)
+
+sequences = ['Re', 'Pt', 'Cc', 'Ea', 'Tb', 'Cm', 'Ex']
+
+for sequence in sequences:
+    cnt = dic_counter[sequence]
+    print(f'{sequence} {cnt} {cnt / total:.2f}')
+
+print(f'Total {total} 1.00')
