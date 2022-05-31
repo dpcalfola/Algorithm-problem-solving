332a8c19347ed12f20f73fffe80d7ae7398138af
Fola Flor <fola_studio@Folaui-Mac-Studio.local>
2022-05-31 16:16:11 +0900

    python ) baekjoon 2022_05_31

--- baekjoon_pythone/baekjoon_2022y/q10101.py
+++ baekjoon_pythone/baekjoon_2022y/q10101.py
@@ -11,19 +11,8 @@ import sys
 
 angles = []
 
-# input_a = int(sys.stdin.readline().rstrip())
-# print(input_a)
-# input_b = int(sys.stdin.readline().rstrip())
-# print(input_b)
-# input_c = int(sys.stdin.readline().rstrip())
-#
-# angles.append(input_a)
-# angles.append(input_b)
-# angles.append(input_c)
-
-
 for _ in range(3):
-    input_number = int(sys.stdin.readline().rstrip())
+    input_number: int = int(sys.stdin.readline().rstrip())
     angles.append(input_number)
 
 
